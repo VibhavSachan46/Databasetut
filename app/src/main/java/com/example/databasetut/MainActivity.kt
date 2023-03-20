3c93456222d1e767013274d6a4d01a870e4f5fff
Vibhav <vibhavsachan743@gmail.com>
2023-03-20 22:49:37 +0530

    Firebase authentication completed

--- app/src/main/java/com/example/databasetut/MainActivity.kt
+++ app/src/main/java/com/example/databasetut/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
@@ -15,21 +18,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val signupButton = findViewById<Button>(R.id.btnSignup)
-        val etname = findViewById<EditText>(R.id.etName)
-        val etMail = findViewById<EditText>(R.id.etmail)
-        val userId = findViewById<EditText>(R.id.etuniqueId)
-        val etpassworrd = findViewById<EditText>(R.id.etpassword)
+        val etname = findViewById<TextInputEditText>(R.id.etName)
+        val etMail = findViewById<TextInputEditText>(R.id.etmail)
+        val userId = findViewById<TextInputEditText>(R.id.etuniqueId)
+        val etpassworrd = findViewById<TextInputEditText>(R.id.etpassword)
 
         signupButton.setOnClickListener {
-            val name = etname.text
-            val mail = etMail.text
-            val userid =userId.text
-            val  password = etpassworrd.text
+            val name = etname.text.toString()
+            val mail = etMail.text.toString()
+            val uniqueId =userId.text.toString()
+            val  password = etpassworrd.text.toString()
 
+            val user = user(name ,mail, password, uniqueId)
             database = FirebaseDatabase.getInstance().getReference("users")
+             //from user.kt
 
+            database.child(uniqueId).setValue(user).addOnSuccessListener {
+                etname.text?.clear()
+                etMail.text?.clear()
+                userId.text?.clear()
+                etpassworrd.text?.clear()
+                Toast.makeText(this,"Uset registered",Toast.LENGTH_SHORT).show()
+            }
 
         }
-
     }
 }
